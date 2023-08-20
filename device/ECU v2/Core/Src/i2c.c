9e108fae999129fe493f9d74f47c9acfcf468e52
LUFT-AQUILA <mail@luftaquila.io>
2023-08-20 22:37:50 +0900

    update: 10kHz LCD I2C freq

--- device/ECU v2/Core/Src/i2c.c
+++ device/ECU v2/Core/Src/i2c.c
@@ -310,7 +310,7 @@ void MX_I2C2_Init(void)
 
   /* USER CODE END I2C2_Init 1 */
   hi2c2.Instance = I2C2;
-  hi2c2.Init.ClockSpeed = 50000;
+  hi2c2.Init.ClockSpeed = 10000;
   hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c2.Init.OwnAddress1 = 0;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
